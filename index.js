4154850d902c835198c46c980e569be783968c35
Focusedplumes <152942773+Focusedplumes@users.noreply.github.com>
2024-04-17 18:01:21 -0500

    initial push to repo of our color console.

---
+++ index.js
@@ -0,0 +1,4 @@
+import { color, log, red, green } from 'console-log-colors';
+
+console.log(color.red("this is a really RED boring dumb message"));
+console.log(color.green("this is a really GREEN boring dumb message"));
